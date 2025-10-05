57d06b1a3aba7206a26a075ae815fcb117cd38e4
terist999 <xscammer25@gmail.com>
2025-10-05 20:50:42 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/yJZeCLCd"))()
+loadstring(game:HttpGet("https://pastefy.app/0dy9zCvH/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
